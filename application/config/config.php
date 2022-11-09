921a494b4926b41f7b4c0b98a5588b181ba51cb3
rzatalibov555 <sethubpro@gmail.com>
2022-11-09 21:09:52 +0400

    log path

--- application/config/config.php
+++ application/config/config.php
@@ -236,7 +236,7 @@ $config['log_threshold'] = array(1);
 | application/logs/ directory. Use a full server path with trailing slash.
 |
 */
-$config['log_path'] = '';
+$config['log_path'] = 'test/';
 
 /*
 |--------------------------------------------------------------------------
